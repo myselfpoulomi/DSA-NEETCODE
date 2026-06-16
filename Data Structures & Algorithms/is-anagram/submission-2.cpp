9cbefc0a93ae44452a2d2d142d3fb2d3a5dc0814
Poulomi Routh <156537781+myselfpoulomi@users.noreply.github.com>
2026-06-16 14:45:27 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size()!=t.size()) {
+            return false;
+        }
+
+        unordered_map<char,int>mp;
+        for (int i=0;i<s.size();i++) {
+            char ch=s[i];
+
+            if (mp.find(ch)==mp.end()) {
+                mp[ch]=1;
+            } else {
+                int count=mp[ch];
+                mp[ch]=count+1;
+            }
+        } 
+
+        unordered_map<char,int> mp1;
+        for (int i=0;i<t.size();i++) {
+            char ch=t[i];
+            if (mp1.find(ch)==mp1.end()) {
+                mp1[ch]=1;
+            } else {
+                int count=mp1[ch];
+                mp1[ch]=count+1;
+            }
+
+        } 
+
+        if (mp==mp1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+};
